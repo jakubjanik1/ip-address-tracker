db181aef982b43dd475af509dfd5537cb88827a8
Jakub Janik <jakubjanik00@gmail.com>
2020-09-03 06:47:40 +0200

    Set up a React project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function App() {
+    return (
+        <div>
+            IP Address Tracker
+
+            Search for any IP address or domain
+
+            IP Address
+            Location
+            Timezone
+            UTC (add offset value dynamically using the API)
+            ISP
+        </div>
+    )
+}
