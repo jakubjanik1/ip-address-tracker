32431daa40fc2fdbeff9882c9917de30e414c499
Jakub Janik <jakubjanik00@gmail.com>
2020-09-14 18:32:09 +0200

    Render a map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { SearchAddress } from './components/SearchAddress'
-import styled,{ createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
+import { Map, TileLayer } from 'react-leaflet'
 
 const GlobalStyle = createGlobalStyle`
     body {
         background: url('img/pattern-bg.png') no-repeat;
-        background-size: cover;
+        background-size: 100% auto;
         font-family: 'Rubik', sans-serif; 
         margin: 0;
     }
@@ -29,6 +30,12 @@ export default function App() {
             <Container>
                 <Header>IP Address Tracker</Header>
                 <SearchAddress />
+                <Map center={[39.95, 30.33]} zoom={13}  style={{ width: '100%', height: 'calc(100vh - 250px)', marginTop: '113px'}}>
+                    <TileLayer 
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                </Map>
             </Container>
         </>
     )
