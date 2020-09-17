581c89215a41aee492b881d510f1df4fe056407e
Jakub Janik <44313733+jakubjanik1@users.noreply.github.com>
2020-09-17 12:06:06 +0200

    Extract map into LocationMap component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { SearchAddress } from './components/SearchAddress'
 import styled, { createGlobalStyle } from 'styled-components'
-import { Map, TileLayer } from 'react-leaflet'
+import { LocationMap } from './components/LocationMap'
 
 const GlobalStyle = createGlobalStyle`
     body {
         background: url('img/pattern-bg.png') no-repeat;
-        background-size: 100% auto;
+        background-size: 100% 250px;
         font-family: 'Rubik', sans-serif; 
         margin: 0;
     }
@@ -30,12 +30,7 @@ export default function App() {
             <Container>
                 <Header>IP Address Tracker</Header>
                 <SearchAddress />
-                <Map center={[39.95, 30.33]} zoom={13}  style={{ width: '100%', height: 'calc(100vh - 250px)', marginTop: '113px'}}>
-                    <TileLayer 
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
-                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    />
-                </Map>
+                <LocationMap latitude={49.60} longitude={20.69} />
             </Container>
         </>
     )
---
+++ src/components/LocationMap.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Map, TileLayer, Marker} from 'react-leaflet'
+import {Icon} from 'leaflet'
+import styled from 'styled-components'
+
+const StyledMap = styled(Map)`
+  width: 100%;
+  height: calc(100vh - 250px);
+  margin-top: 113px;
+`
+
+function LocationMap({latitude, longitude}) {
+  const coordinates = [latitude, longitude]
+
+  return (
+    <StyledMap 
+      zoomControl={false}
+      center={coordinates} 
+      zoom={14} 
+    >
+      <Marker 
+        position={coordinates} 
+        icon={new Icon({iconUrl: 'img/icon-location.svg'})}
+      />
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+    </StyledMap>
+  )
+}
+
+export {LocationMap}
