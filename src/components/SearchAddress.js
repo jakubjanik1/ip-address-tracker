f40da2fd5e6478f1c660a08d4a7ef4f0c75a06d0
Jakub Janik <jakubjanik00@gmail.com>
2020-09-22 18:37:46 +0200

    Make some code style changes

--- src/components/SearchAddress.js
+++ src/components/SearchAddress.js
@@ -2,51 +2,53 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Form = styled.form`
-    display: flex;
+  display: flex;
 `
 
 const Input = styled.input`
-    font-size: 18px;
-    font-family: inherit;
-    color: hsl(0, 0%, 17%);
-    padding: 1rem 1.5rem;
-    border: 0;
-    border-radius: 1rem 0 0 1rem;
-    width: 500px;
+  font-size: 18px;
+  font-family: inherit;
+  color: hsl(0, 0%, 17%);
+  padding: 1rem 1.5rem;
+  border: 0;
+  border-radius: 1rem 0 0 1rem;
+  width: 500px;
 
-    &::placeholder {
-        color: hsl(0, 0%, 59%);
-    }
+  &::placeholder {
+    color: hsl(0, 0%, 59%);
+  }
 
-    &:focus {
-        outline: transparent;
-    }
+  &:focus {
+    outline: transparent;
+  }
 `
 
 const Button = styled.button`
-    background: #000;
-    padding: 1rem 1.5rem;
-    border: 0;
-    font-size: 18px;
-    border-radius: 0 1rem 1rem 0;
+  background: #000;
+  padding: 1rem 1.5rem;
+  border: 0;
+  font-size: 18px;
+  border-radius: 0 1rem 1rem 0;
 
-    &:focus {
-        outline: transparent;
-    }
+  &:focus {
+    outline: transparent;
+  }
 `
 
-export const SearchAddress = ({ onSubmit }) => {
-    const handleSubmit = e => {
-        e.preventDefault()
-        onSubmit(e.target.elements.address.value)
-    }
+function SearchAddress({onSubmit}) {
+  function handleSubmit(e) {
+    e.preventDefault()
+    onSubmit(e.target.elements.address.value)
+  }
 
-    return (
-        <Form onSubmit={handleSubmit}>
-            <Input aria-label="address" name="address" placeholder="Search for any IP address or domain" />
-            <Button type="submit">
-                <img src="img/icon-arrow.svg" alt="arrow left"/>
-            </Button>
-        </Form> 
-    )
+  return (
+      <Form onSubmit={handleSubmit}>
+          <Input aria-label="address" name="address" placeholder="Search for any IP address or domain" />
+          <Button type="submit">
+              <img src="img/icon-arrow.svg" alt="arrow left"/>
+          </Button>
+      </Form> 
+  )
 }
+
+export {SearchAddress}
--- src/components/__tests__/SearchAddress.js
+++ src/components/__tests__/SearchAddress.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import { SearchAddress } from '../SearchAddress'
-import { render, screen } from '@testing-library/react'
+import {SearchAddress} from '../SearchAddress'
+import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
 test('submitting the form calls onSubmit with address', () => {
-    const address = '172.156.23.12'
-    const handleSubmit = jest.fn()
+  const address = '172.156.23.12'
+  const handleSubmit = jest.fn()
 
-    render(<SearchAddress onSubmit={handleSubmit} />)
+  render(<SearchAddress onSubmit={handleSubmit} />)
 
-    userEvent.type(screen.getByLabelText(/address/i), address)
-    userEvent.click(screen.getByRole('button'))
+  userEvent.type(screen.getByLabelText(/address/i), address)
+  userEvent.click(screen.getByRole('button'))
 
-    expect(handleSubmit).toHaveBeenCalledWith(address)
-    expect(handleSubmit).toHaveBeenCalledTimes(1)
+  expect(handleSubmit).toHaveBeenCalledWith(address)
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
 })
