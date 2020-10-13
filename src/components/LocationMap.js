91014c8bf0110fcf88f08462815a7606bcbd631a
Jakub Janik <44313733+jakubjanik1@users.noreply.github.com>
2020-10-13 10:10:15 +0200

    Add loading spinner to LocationMap

--- src/components/LocationMap.js
+++ src/components/LocationMap.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Map, TileLayer, Marker} from 'react-leaflet'
 import {Icon} from 'leaflet'
 import styled from 'styled-components'
+import Loader from 'react-spinners/PulseLoader'
 
 const StyledMap = styled(Map)`
   width: 100%;
@@ -10,8 +11,22 @@ const StyledMap = styled(Map)`
   margin-top: auto;
 `
 
-function LocationMap({latitude = 0, longitude = 0}) {
+const Loading = styled.div`
+  width: 100%;
+  height: calc(100vh - 16.5rem);
+  z-index: 0;
+  margin-top: auto;
+  background: #eee;
+  display: grid;
+  place-items: center;
+`
+
+function LocationMap({latitude, longitude}) {
   const coordinates = [latitude, longitude]
+
+  if (!latitude) {
+    return <Loading><Loader /></Loading>
+  }
 
   return (
     <StyledMap 
